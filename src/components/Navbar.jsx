eb681c4a428847afa7e3d9fc65337aa167f3c75e
Ryan Rumao <ryanrumao4@gmail.com>
2025-03-05 20:26:42 -0600

    navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getCurrentUser, signOut } from "@aws-amplify/auth";
-import MultiCenterGradient from "./gradient";
 
 const styles = {
     container: {
