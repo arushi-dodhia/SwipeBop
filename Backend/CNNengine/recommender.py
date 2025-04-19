3fd95d521779b94221d1006b366062b13b1d5cc2
Raihan Tanvir <raihanntanvir@gmail.com>
2025-04-18 19:58:52 -0500

    testing hybrid rec

--- Backend/CNNengine/recommender.py
+++ Backend/CNNengine/recommender.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+from random import sample
+
 
 EMBEDDINGS_DIR = "/home/ec2-user/SwipeBop/Backend/CNNengine/embeddings"
 # EMBEDDINGS_DIR = "/Users/raihan/Desktop/classes/SwipeBop/Backend/CNNengine/embeddings"
@@ -20,11 +22,28 @@ def build_user_embedding(liked_product_sins, catalog_embeddings):
         return None
     return np.mean(liked_embs, axis=0)
 
-def recommend_products(user_emb, catalog_embeddings, exclude_ids=[], top_n=10):
-    scores = {}
-    for pid, emb in catalog_embeddings.items():
-        if pid in exclude_ids:
-            continue
-        sim = cosine_similarity([user_emb], [emb])[0][0]
-        scores[pid] = sim
-    return sorted(scores.items(), key=lambda x: x[1], reverse=True)[:top_n]
+# cosine similarity, problem is it will recommend same items everytime
+# def recommend_products(user_emb, catalog_embeddings, exclude_ids=[], top_n=10):
+#     scores = {}
+#     for pid, emb in catalog_embeddings.items():
+#         if pid in exclude_ids:
+#             continue
+#         sim = cosine_similarity([user_emb], [emb])[0][0]
+#         scores[pid] = sim
+#     return sorted(scores.items(), key=lambda x: x[1], reverse=True)[:top_n]
+
+def hybrid_recommend(user_emb, catalog, exclude_ids, top_k=20, final_n=10, random_frac=0.3):
+    # get the 20 most similar:
+    top20 = sorted(
+        ((pid, cosine_similarity([user_emb],[emb])[0][0]) for pid,emb in catalog.items()
+         if pid not in exclude_ids),
+        key=lambda x: x[1], reverse=True
+    )[:top_k]
+    top_pids = [pid for pid,_ in top20]
+    # pick 70% from the very best, 30% at random from the rest:
+    from math import floor
+    best_n = floor(final_n * (1 - random_frac))
+    recs = sample(top_pids[:best_n], best_n)
+    # and  the rest from the lower‐ranked top20
+    other = sample(top_pids[best_n:], final_n - best_n)
+    return recs + other
